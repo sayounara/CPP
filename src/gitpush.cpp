ca892ecbaf5a3b2be516cf2334b5ac7574312622
HeXin <sayounara@hnu.edu.cn>
2015-07-16 15:16:46 +0800

    commit gitpush.cpp

---
+++ src/gitpush.cpp
@@ -0,0 +1,16 @@
+/*
+ * gitpush.cpp
+ *
+ *  Created on: 2015年7月16日
+ *      Author: hexin
+ */
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv) {
+	cout<<"Hello ,Git"<<endl;
+	return 0;
+}
+
+
+
+
